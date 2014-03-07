619cd4f67607995a51ab8d1051171d56a917dce8
Luis Abreu <lmjabreu@gmail.com>
2014-03-07 16:28:21 +0000

    Initial Commit

---
+++ ColorBeacon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorBeacon
+//
+//  Created by Luis Abreu on 07/03/2014.
+//  Copyright (c) 2014 lmjabreu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorBeaconTests/ColorBeaconTests.m
@@ -0,0 +1,34 @@
+//
+//  ColorBeaconTests.m
+//  ColorBeaconTests
+//
+//  Created by Luis Abreu on 07/03/2014.
+//  Copyright (c) 2014 lmjabreu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ColorBeaconTests : XCTestCase
+
+@end
+
+@implementation ColorBeaconTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
